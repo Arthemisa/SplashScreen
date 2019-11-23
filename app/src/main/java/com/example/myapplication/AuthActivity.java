8cbb11c399ced3e4368adcef3778b08b82028411
unknown <Admin@IHR80PBF251.continental.edu.pe>
2019-11-23 13:03:41 -0500

    cambios

--- app/src/main/java/com/example/myapplication/AuthActivity.java
+++ app/src/main/java/com/example/myapplication/AuthActivity.java
@@ -21,16 +21,18 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
         btnLogin = findViewById(R.id.btnLogin);
         txtUser = findViewById(R.id.txtUser);
+        txtPassword = findViewById(R.id.txtPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( txtUser.getText().toString().equals("alcachofa") ) {
+                if ( txtUser.getText().toString().equals("alcachofa" ) &&  txtPassword.getText().toString().equals("alcachofa" ) ) {
                     Intent o = new Intent(AuthActivity.this,
                             MainActivity.class);
                     startActivity(o);
                 }
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent o = new Intent(this, AuthActivity.class);
+        startActivity(o);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
